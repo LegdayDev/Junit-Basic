3f4c073b30f62575bad3e85bec9b3b9e7860939d
Legday <chlwodud0327@naver.com>
2023-11-20 14:55:06 +0900

    Feat: BookService - 책 목록보기
    findAll() 이 반환하는 값은 List<Book>이므로 DTO로 변경이 필요하다
    Java 8 이 지원하는 stream() 을 이용하여 간편하게 List<BookRespDtO> 로 변환가능하다.

--- src/main/java/site/metacoding/junitproject/service/BookService.java
+++ src/main/java/site/metacoding/junitproject/service/BookService.java
@@ -1,26 +1,38 @@
 package site.metacoding.junitproject.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.junitproject.domain.Book;
 import site.metacoding.junitproject.domain.BookRepository;
+import site.metacoding.junitproject.web.dto.BookRespDto;
 import site.metacoding.junitproject.web.dto.BookSaveReqDto;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class BookService {
 
     private final BookRepository bookRepository;
 
     // 1. 책 등록
-    public Book 책등록하기(BookSaveReqDto dto) {
-        // DTO -> Entity -> DTO ?
+    @Transactional(rollbackFor = RuntimeException.class)
+    public BookRespDto 책등록하기(BookSaveReqDto dto) {
         Book bookPS = bookRepository.save(dto.toEntity());
 
-        return bookPS;
+        return new BookRespDto().toDto(bookPS);
     }
+
     // 2. 책 목록보기
+    public List<BookRespDto> 책목록보기(){
+        return bookRepository.findAll().stream()
+                    .map(new BookRespDto()::toDto)
+                    .collect(Collectors.toList());
+    }
 
     // 3. 책 한건보기
 
