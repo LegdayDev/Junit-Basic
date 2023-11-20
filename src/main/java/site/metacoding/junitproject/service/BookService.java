656e7545978e01755afd0dd065298086375dffc8
Legday <chlwodud0327@naver.com>
2023-11-20 15:02:48 +0900

    Feat: BookService - 책 한건보기
    findById()로 Entity를 찾으면 NullPointerException 을 방지하기 위해 Optional로 감싸져서 반환된다.
    isPresent()로 값의 유무를 따져 값이 있으면 DTO로 변환하여 반환하고, 없으면 RuntimeException을 throw 해주면 된다.

--- src/main/java/site/metacoding/junitproject/service/BookService.java
+++ src/main/java/site/metacoding/junitproject/service/BookService.java
@@ -1,6 +1,7 @@
 package site.metacoding.junitproject.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
@@ -28,13 +29,21 @@ public class BookService {
     }
 
     // 2. 책 목록보기
-    public List<BookRespDto> 책목록보기(){
+    public List<BookRespDto> 책목록보기() {
         return bookRepository.findAll().stream()
-                    .map(new BookRespDto()::toDto)
-                    .collect(Collectors.toList());
+                .map(new BookRespDto()::toDto)
+                .collect(Collectors.toList());
     }
 
     // 3. 책 한건보기
+    public BookRespDto 책한건보기(Long id) {
+        Optional<Book> bookOP = bookRepository.findById(id);
+        if (bookOP.isPresent()) {
+            return new BookRespDto().toDto(bookOP.get());
+        } else {
+            throw new RuntimeException("해당 아이디를 찾을 수 없습니다.");
+        }
+    }
 
     // 4. 책 삭제하기
 
