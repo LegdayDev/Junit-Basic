9a88aa8addce605e2a6353dde6e75531c9b67d0d
Legday <chlwodud0327@naver.com>
2023-11-20 14:34:06 +0900

    Feat: BookService - 책 등록 구현
    BookReqDto 로 데이터를 받고 Entity로 변환후 save() 한다.
    save()로 반환된 Entity를 다시 BookRespDto로 변환하여 응답한다.

--- src/main/java/site/metacoding/junitproject/service/BookService.java
+++ src/main/java/site/metacoding/junitproject/service/BookService.java
@@ -2,7 +2,30 @@ package site.metacoding.junitproject.service;
 
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+import site.metacoding.junitproject.domain.Book;
+import site.metacoding.junitproject.domain.BookRepository;
+import site.metacoding.junitproject.web.dto.BookSaveReqDto;
+
 @Service
+@RequiredArgsConstructor
 public class BookService {
+
+    private final BookRepository bookRepository;
+
+    // 1. 책 등록
+    public Book 책등록하기(BookSaveReqDto dto) {
+        // DTO -> Entity -> DTO ?
+        Book bookPS = bookRepository.save(dto.toEntity());
+
+        return bookPS;
+    }
+    // 2. 책 목록보기
+
+    // 3. 책 한건보기
+
+    // 4. 책 삭제하기
+
+    // 5. 책 수정하기
 
 }
--- src/main/java/site/metacoding/junitproject/web/dto/BookRespDto.java
+++ src/main/java/site/metacoding/junitproject/web/dto/BookRespDto.java
@@ -1,5 +1,19 @@
 package site.metacoding.junitproject.web.dto;
 
-public class BookRespDto {
+import lombok.Data;
+import site.metacoding.junitproject.domain.Book;
 
+@Data
+public class BookRespDto {
+    private Long id;
+    private String title;
+    private String author;
+
+    public BookRespDto toDto(Book bookPS) {
+        this.id = bookPS.getId();
+        this.title = bookPS.getTitle();
+        this.author = bookPS.getAuthor();
+
+        return this;
+    }
 }
