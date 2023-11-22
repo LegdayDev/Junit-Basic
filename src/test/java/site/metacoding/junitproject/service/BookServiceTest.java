9917f8997a03124704dbad0f16c964112ce5a3df
Legday <chlwodud0327@naver.com>
2023-11-22 18:46:45 +0900

    Feat: BookServiceTest - 책등록(Mockito 환경)
    BookRepository , MailSender 는 Mock 환경에 로딩하고 BookService 를 메모리에 띄워서 Mock환경에 있는 가짜 클래스들을 실제 BookService 에 주입받아서 테스트한다.
    그리고 when() 메서드로 가설을 정하고 검증을 하였다.

--- src/test/java/site/metacoding/junitproject/service/BookServiceTest.java
+++ src/test/java/site/metacoding/junitproject/service/BookServiceTest.java
@@ -1,21 +1,32 @@
 package site.metacoding.junitproject.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import site.metacoding.junitproject.domain.BookRepository;
-import site.metacoding.junitproject.util.MailSenderStub;
+import site.metacoding.junitproject.util.MailSender;
 import site.metacoding.junitproject.web.dto.BookRespDto;
 import site.metacoding.junitproject.web.dto.BookSaveReqDto;
 
-@DataJpaTest
+@ExtendWith(MockitoExtension.class)
 public class BookServiceTest {
 
-    @Autowired
+    // BookService 객체가 heap 메모리에 뜨고 BookService 가 의존하는 BookRepository , MailSender를
+    // BookService 에 주입 시켜준다.
+    @InjectMocks
+    private BookService bookService;
+
+    @Mock // 가짜 환경에 BookRepository , MailSender 를 로딩함.
     private BookRepository bookRepository;
+    @Mock
+    private MailSender mailSender;
 
     @Test
     public void 책등록하기_test() throws Exception {
@@ -23,11 +34,12 @@ public class BookServiceTest {
         BookSaveReqDto dto = new BookSaveReqDto();
         dto.setTitle("Football");
         dto.setAuthor("Cristiano");
-        // stub
-        MailSenderStub mailSenderStub = new MailSenderStub();
+
+        // stub(가설)
+        when(bookRepository.save(any())).thenReturn(dto.toEntity());
+        when(mailSender.send()).thenReturn(true);
 
         // when
-        BookService bookService = new BookService(bookRepository, mailSenderStub);
         BookRespDto bookRespDto = bookService.책등록하기(dto);
 
         // then
