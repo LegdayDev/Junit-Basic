ba678f54563950ebdded5dd3e930c8e097e1ce9d
Legday <chlwodud0327@naver.com>
2023-11-22 17:14:57 +0900

    Feat: 가짜 이메일 보내기 기능 추가
    Mail 클래스를 다른 개발자 만들고  가짜 메일 기능을 인터페이스로 연결한다.

---
+++ src/main/java/site/metacoding/junitproject/util/Mail.java
@@ -0,0 +1,11 @@
+package site.metacoding.junitproject.util;
+
+// 아직 구현되지 않음
+/*
+ * public class Mail {
+ * public boolean sendMail() {
+ * // 기능
+ * return true;
+ * }
+ * }
+ */
---
+++ src/main/java/site/metacoding/junitproject/util/MailSenderAdapter.java
@@ -0,0 +1,17 @@
+package site.metacoding.junitproject.util;
+
+public class MailSenderAdapter implements MailSender {
+
+    /*
+     * private Mail mail;
+     * 
+     * public MailSenderAdapter(){
+     * this.mail = new Mail();
+     * }
+     */
+
+    @Override
+    public boolean send() {
+        return true;
+    }
+}
