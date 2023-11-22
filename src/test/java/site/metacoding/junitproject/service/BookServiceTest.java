39299456622b9d265348b72a1a032fe8dedd5a44
Legday <chlwodud0327@naver.com>
2023-11-22 18:06:24 +0900

    Feat: BookServiceTest - 책등록
    실제 Repository 를 메모리에 띄워놓고 테스트

--- src/test/java/site/metacoding/junitproject/service/BookServiceTest.java
+++ src/test/java/site/metacoding/junitproject/service/BookServiceTest.java
@@ -1,5 +1,38 @@
 package site.metacoding.junitproject.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import site.metacoding.junitproject.domain.BookRepository;
+import site.metacoding.junitproject.util.MailSenderStub;
+import site.metacoding.junitproject.web.dto.BookRespDto;
+import site.metacoding.junitproject.web.dto.BookSaveReqDto;
+
+@DataJpaTest
 public class BookServiceTest {
 
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Test
+    public void 책등록하기_test() throws Exception {
+        // given
+        BookSaveReqDto dto = new BookSaveReqDto();
+        dto.setTitle("Football");
+        dto.setAuthor("Cristiano");
+        // stub
+        MailSenderStub mailSenderStub = new MailSenderStub();
+
+        // when
+        BookService bookService = new BookService(bookRepository, mailSenderStub);
+        BookRespDto bookRespDto = bookService.책등록하기(dto);
+
+        // then
+        assertThat(dto.getTitle()).isEqualTo(bookRespDto.getTitle());
+        assertThat(dto.getAuthor()).isEqualTo(bookRespDto.getAuthor());
+
+    }
 }
