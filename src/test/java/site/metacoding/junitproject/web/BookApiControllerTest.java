56cb78c1b5697eeb3b905ac8ec42ee263980824e
Legday <chlwodud0327@naver.com>
2023-11-25 14:54:01 +0900

    Feat: BookApiControllerTest - 책 목록보기
    GET 방식이므로 HTTP body는 없이 통신하여 테스트 하면 된다.

--- src/test/java/site/metacoding/junitproject/web/BookApiControllerTest.java
+++ src/test/java/site/metacoding/junitproject/web/BookApiControllerTest.java
@@ -5,11 +5,14 @@ import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import site.metacoding.junitproject.domain.Book;
+import site.metacoding.junitproject.domain.BookRepository;
 import site.metacoding.junitproject.service.BookService;
 import site.metacoding.junitproject.web.dto.request.BookSaveReqDto;
 
@@ -27,6 +30,8 @@ public class BookApiControllerTest {
     private BookService bookService;
     @Autowired
     private TestRestTemplate rt;
+    @Autowired
+    private BookRepository bookRepository;
 
     private static ObjectMapper om;
     private static HttpHeaders headers;
@@ -35,6 +40,13 @@ public class BookApiControllerTest {
         om = new ObjectMapper();
         headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
+    }
+    @BeforeEach
+    public void dataInit() {
+        String title = "This is Football";
+        String author = "Cristiano Ronaldo";
+        Book book = Book.builder().title(title).author(author).build();
+        bookRepository.save(book);
     }
 
     @Test
@@ -57,5 +69,25 @@ public class BookApiControllerTest {
 
         assertThat(title).isEqualTo(dto.getTitle());
         assertThat(author).isEqualTo(dto.getAuthor());
+    }
+
+    @Test
+    public void getBookListTest() throws Exception {
+        //given
+
+        //when
+        HttpEntity<String> request = new HttpEntity<>(null, headers); //GET 은 HTTP Body가 없다.
+        ResponseEntity<String> response = rt.exchange("/api/v1/book", HttpMethod.GET, request, String.class);
+
+        //then
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        Integer code = documentContext.read("$.code");
+        String title = documentContext.read("$.body.data[0].title");
+        String author = documentContext.read("$.body.data[0].author");
+
+        assertThat(code).isEqualTo(1);
+        assertThat(title).isEqualTo("This is Football");
+        assertThat(author).isEqualTo("Cristiano Ronaldo");
+
     }
 }
